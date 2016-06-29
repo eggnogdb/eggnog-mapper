315c6ff1a64d22962d331ab2f9061cd10becb43f
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-06-29 17:55:22 +0200

    clean up old file

--- download_eggnog_data.py
+++ download_eggnog_data.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python
 import sys
+import os
 from eggnogmapper.common import EGGNOG_DATABASES, DATA_PATH
+
+def ask(string, valid_values, default=-1, case_sensitive=False):
+    """ Asks for a keyborad answer """
+    v = None
+    if not case_sensitive:
+        valid_values = [value.lower() for value in valid_values]
+    while v not in valid_values:
+        v = raw_input("%s [%s]" % (string,','.join(valid_values) ))
+        if v == '' and default >= 0:
+            v = valid_values[default]
+        if not case_sensitive:
+            v = v.lower()
+    return v
 
 def download_hmm_data9base(level):
     if level == 'euk':
@@ -23,6 +38,6 @@ def download_groups():
     cmd = 'cd %s && wget -nH --user-agent=Mozilla/5.0 --relative --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off -O OG_fasta.tar.gz  %s && tar -zxf OG_fasta.tar.gz && rm OG_fasta.tar.gz' %(DATA_PATH,  url)
     return cmd
 
-    
+print download_groups("euk")
 print download_groups()
 print download_annotations()
