c070a02445632a868d9b03428504a76a0d630773
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-06-29 17:51:18 +0200

    improving data download

--- download_eggnog_data.py
+++ download_eggnog_data.py
@@ -1,7 +1,7 @@
 import sys
 from eggnogmapper.common import EGGNOG_DATABASES, DATA_PATH
 
-def download_hmm_database(level):
+def download_hmm_data9base(level):
     if level == 'euk':
         level = 'euk_500'
     elif level == 'bact':
@@ -14,13 +14,13 @@ def download_hmm_database(level):
     return cmd
 
 def download_annotations():
-    url = 'http://beta-eggnogdb.embl.de/download/eggnog_4.5/eggnog-mapper-data/eggnog.db' 
-    cmd = 'cd %s; wget -nH --user-agent=Mozilla/5.0 --relative --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off -O eggnog.db -f %s' %(DATA_PATH, url)
+    url = 'http://beta-eggnogdb.embl.de/download/eggnog_4.5/eggnog-mapper-data/eggnog.db.gz' 
+    cmd = 'cd %s && wget -nH --user-agent=Mozilla/5.0 --relative --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off -O eggnog.db.gz %s && gunzip eggnog.db.gz' %(DATA_PATH, url)
     return cmd
     
 def download_groups():
-    url = 'http://beta-eggnogdb.embl.de/download/eggnog_4.5/eggnog-mapper-data/OG_data.tar.gz' 
-    cmd = 'cd %s; wget -nH --user-agent=Mozilla/5.0 --relative --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off -O OG_data.tar.gz -f %s && tar -zxf OG_data.tar.gz' %(DATA_PATH,  url)
+    url = 'http://beta-eggnogdb.embl.de/download/eggnog_4.5/eggnog-mapper-data/OG_fasta.tar.gz' 
+    cmd = 'cd %s && wget -nH --user-agent=Mozilla/5.0 --relative --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off -O OG_fasta.tar.gz  %s && tar -zxf OG_fasta.tar.gz && rm OG_fasta.tar.gz' %(DATA_PATH,  url)
     return cmd
 
     
