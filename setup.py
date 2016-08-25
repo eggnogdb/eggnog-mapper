270a80129da6355856b00719888159e7172a0d2a
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-08-25 11:17:28 +0200

    add basic setup script

---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+    
+#HERE = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
+
+CLASSIFIERS= [
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Natural Language :: English",
+    "Operating System :: MacOS",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+
+    
+MOD_NAME = "eggnogmapper"
+VERSION = '1.0'
+LONG_DESCRIPTION="""
+Functional annotation of novel sequences using eggNOG orthology assignments.
+"""
+
+try:
+    _s = setup(
+        include_package_data = False,
+
+        name = MOD_NAME,
+        version = VERSION,
+        packages = ['eggnogmapper'],
+
+        # Project uses reStructuredText, so ensure that the docutils get
+        # installed or upgraded on the target machine
+        install_requires = [
+            ],
+        package_data = {
+
+        },
+        data_files = [],
+
+        # metadata for upload to PyPI
+        author = "Jaime Huerta-Cepas",
+        author_email = "jhcepas@gmail.com",
+        maintainer = "Jaime Huerta-Cepas",
+        maintainer_email = "huerta@embl.de",
+        platforms = "OS Independent",
+        license = "GPLv3",
+        description = "A Python Environment for (phylogenetic) Tree Exploration",
+        long_description = LONG_DESCRIPTION,
+        classifiers = CLASSIFIERS,
+        provides = [MOD_NAME],
+        keywords = "functional annotation, orthology, eggNOG",
+        url = "http://eggnogdb.embl.de",
+    )
+
+except:
+    print("\033[91m - Errors found! - \033[0m")
+    raise
