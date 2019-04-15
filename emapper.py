b652ff95324839998ff314d3d67209c291dcc58e
Jaime Huerta-Cepas <jhcepas@gmail.com>
2019-04-15 12:03:31 +0200

    fixes help messages

--- emapper.py
+++ emapper.py
@@ -1103,10 +1103,10 @@ if __name__ == "__main__":
                     help='Gap extend  penalty')
 
     pg_diamond.add_argument('--query-cover', dest='query_cover', type=float, default=0,
-                    help='Report only alignments above the given percentage of query cover. Default=25')
+                    help='Report only alignments above the given percentage of query cover. Default=0')
 
     pg_diamond.add_argument('--subject-cover', dest='subject_cover', type=float, default=0,
-                    help='Report only alignments above the given percentage of subject cover. Default=25')
+                    help='Report only alignments above the given percentage of subject cover. Default=0')
 
     pg_seed = parser.add_argument_group('Seed ortholog search option')
 
