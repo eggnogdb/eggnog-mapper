f629f2303abede42d9c653c870011efe7d879759
Cantalapiedra <gargamelle@gmail.com>
2020-04-15 16:13:51 +0200

    emapper cmd test

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,25 @@
+##
+## JHCepas
+## CPCantalapiedra 2020
+
 import unittest
 import subprocess
-import os
+import os, shutil
+
+# Settings for this tests
+EMAPPER_CMD = '../emapper.py'
+EXPECTED_SEED_ORTHOLOGS_FN = 'test_output.emapper.seed_orthologs'
+EXPECTED_ANNOTATIONS_FN = 'test_output.emapper.annotations'
+
+# General eggnog-mapper settings
+SEED_ORTHOLOGS_SUFFIX = '.emapper.seed_orthologs'
+ANNOTATIONS_SUFFIX = '.emapper.annotations'
 
 def run(cmd):
-    cmd = './emapper.py ' + cmd
+    '''
+    Runs eggnog-mapper with the arguments specified
+    '''
+    cmd = EMAPPER_CMD + " " + cmd
     print cmd
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = process.communicate()
@@ -13,68 +29,120 @@ def run(cmd):
         err = ''
     return (process.returncode, out, err)
 
+
+def _load_orthologs(orthologs_fn):
+    '''
+    Loads the rows from a seed orthologs output, to a list
+    '''
+    _rows = []
+    with open(orthologs_fn, 'r') as orth_f:
+        for line in orth_f:
+            if line.startswith("#"): continue
+            _rows.append(line)
+    return _rows
+
+def _load_annotations(annotations_fn):
+    '''
+    Loads the rows from an annotations output, to a list
+    '''
+    _rows = []
+    with open(annotations_fn, 'r') as annot_f:
+        for line in annot_f:
+            if line.startswith("#"): continue
+            _rows.append(line)
+    return _rows
+
 class Test(unittest.TestCase):
 
-    def test_annotations(self):
-        os.system('rm polb.emapper.* -f')
-        st, out, err = run('-i test/polb.fa -d bact -o polb')
-        ok = False
-        for line in open("polb.emapper.annotations"):
-            if line.startswith('362663.ECP_0061'):
-                q, hit, evalue, score, pname, gos, ko, bigg, level, groups, bestgroup, cat, desc =  map(str.strip, line.split('\t'))
-                assert hit == '362663.ECP_0061'
-                assert pname == 'POLB'
-                assert set(gos.split(',')) == set("GO:0003674,GO:0003824,GO:0003887,GO:0004518,GO:0004527,GO:0004529,GO:0004536,GO:0005575,GO:0005622,GO:0005623,GO:0005694,GO:0006139,GO:0006259,GO:0006260,GO:0006261,GO:0006281,GO:0006289,GO:0006297,GO:0006301,GO:0006725,GO:0006807,GO:0006950,GO:0006974,GO:0007154,GO:0008150,GO:0008152,GO:0008296,GO:0008408,GO:0009058,GO:0009059,GO:0009432,GO:0009605,GO:0009987,GO:0009991,GO:0016740,GO:0016772,GO:0016779,GO:0016787,GO:0016788,GO:0016796,GO:0016895,GO:0018130,GO:0019438,GO:0019985,GO:0031668,GO:0033554,GO:0034061,GO:0034641,GO:0034645,GO:0034654,GO:0043170,GO:0043226,GO:0043228,GO:0043229,GO:0043232,GO:0044237,GO:0044238,GO:0044249,GO:0044260,GO:0044271,GO:0044424,GO:0044464,GO:0044699,GO:0044763,GO:0045004,GO:0045005,GO:0046483,GO:0050896,GO:0051716,GO:0071496,GO:0071704,GO:0071897,GO:0090304,GO:0090305,GO:1901360,GO:1901362,GO:1901576".split(','))
-                assert ko == 'K02336'
-                assert cat == 'L'
-                assert desc == 'DNA polymerase'
-                ok = True
-        if not ok:
-            ValueError('polB result not obtained')
-        else:
-            os.system('rm polb.emmaper.* -f')
+    def _check_seed_orthologs(self, data_dir, outdir, outprefix):
+        '''
+        Compares the obtained seed orthologs file with the expected one
+        '''
+        # Check that output file has been created
+        out_fn = os.path.join(outdir, outprefix+SEED_ORTHOLOGS_SUFFIX)
+        assert os.path.exists(out_fn)
 
+        # Compare expected and observed output
+        # Load test output
+        out_rows = _load_orthologs(out_fn)
+        
+        # Load expected output
+        exp_fn = os.path.join(data_dir, EXPECTED_SEED_ORTHOLOGS_FN)
+        exp_rows = _load_orthologs(exp_fn)
+
+        # compare both files 
+        self._basic_rows_comparison(out_rows, exp_rows)
+        return
+
+    def _check_annotations(self, data_dir, outdir, outprefix):
+        '''
+        Compares the obtained annotations file with the expected one
+        '''
+        # Check that output file has been created
+        out_fn = os.path.join(outdir, outprefix+ANNOTATIONS_SUFFIX)
+        assert os.path.exists(out_fn)
+
+        # Compare expected and observed output
+        # Load test output
+        out_rows = _load_annotations(out_fn)
+        
+        # Load expected output
+        exp_fn = os.path.join(data_dir, EXPECTED_ANNOTATIONS_FN)
+        exp_rows = _load_annotations(exp_fn)
+
+        # compare both files 
+        self._basic_rows_comparison(out_rows, exp_rows)
+        return
     
-    def test_execution_modes(self):
-        os.system('rm borrame.emapper.* -f')
+    def _basic_rows_comparison(self, l1, l2):
+        '''
+        Performs several basic comparisons between 2 lists
+        '''
+        # check that both lists have the same number of rows
+        assert len(l1) == len(l2)
+        # check that rows are equal one-by-one
+        differences = [i for i, j in zip(l1, l2) if i != j]
+        assert len(differences) == 0
+        return
 
-        # should run
-        st, out, err = run('-i test/polb.fa -d bact:localhost:51500 -o borrame')
-        assert st == 0
+    # Tests
+    def test_emapper_cmd(self):
+        '''
+        Tests the whole emapper command, including the expected output files
+        '''
+        in_file = "fixtures/test_queries.fa"
+        data_dir = "fixtures"
+        outdir = "out"
+        outprefix = "test"
+        
+        # Remove (just in case) and recreate the output dir
+        if os.path.isdir(outdir):
+            shutil.rmtree(outdir)
+        os.mkdir(outdir)
+        
+        cmd = '-m diamond -i %s --data_dir %s --output_dir %s -o %s' % (in_file, data_dir, outdir, outprefix)
 
-        # should fail because output files exist
-        st, out, err = run('-i test/polb.fa -d bact:localhost:51500 -o borrame')
-        assert st == 1
-        assert 'Use --resume' in out
+        print "Running eggnog-mapper..."
+        st, out, err = run(cmd)
+        assert st == 0 # check exit status is ok
 
-        # should run
-        st, out, err = run('-i test/polb.fa -d bact:localhost:51500 -o borrame --override')
-        assert st == 0
+        ##
+        # Check alignment phase: detection of seed orthologs
+        print "Checking seed orthologs..."
+        self._check_seed_orthologs(data_dir, outdir, outprefix)
 
-        # should run with no search
-        st, out, err = run('-i test/polb.fa -d bact:localhost:51500 -o borrame --resume')
-        assert st == 0
+        ##
+        # Check annotation phase
+        print "Checking annotations..."
+        self._check_annotations(data_dir, outdir, outprefix)
 
-        # should run with no search and read prev table
-        st, out, err = run('-i test/polb.fa -d bact:localhost:51500 -o borrame --annotate_hits_table borrame.emapper.seed_orthologs --override')
-        assert st == 0
-
-        # should run from disk
-        os.system('rm borrame.emapper.* -f')
-        st, out, err = run('-i test/polb.fa -d maNOG -o borrame')
-        assert st == 0
-
-        # should run from mem
-        os.system('rm borrame.emapper.* -f')
-        st, out, err = run('-i test/polb.fa -d homNOG -o borrame --usemem')
-        assert st == 0
-
-        # should run from mem
-        os.system('rm borrame.emapper.* -f')
-        st, out, err = run('-i test/polb.fa -o borrame -m diamond --cpu 10')
-        assert st == 0
-
-        os.system('rm borrame.emapper.* -f')
+        # Remove the output dir
+        if os.path.isdir(outdir):
+            shutil.rmtree(outdir)
+        
+        return
 
 if __name__ == '__main__':
     unittest.main()
+
+## END
