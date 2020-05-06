eb85eb5b5fc1681d92f64437c401fa04133a41ca
Cantalapiedra <cpcantalapiedra@gmail.com>
2020-05-06 16:33:55 +0200

    rm servermode option

--- eggnogmapper/search/hmmer_setup.py
+++ eggnogmapper/search/hmmer_setup.py
@@ -19,7 +19,7 @@ SETUP_TYPE_EGGNOG = "eggnog"
 SETUP_TYPE_CUSTOM = "custom"
 
 ##
-def setup_hmm_search(db, scantype, dbtype, no_refine, cpu, servermode):
+def setup_hmm_search(db, scantype, dbtype, no_refine, cpu):
 
     setup_type = None
     
@@ -38,15 +38,7 @@ def setup_hmm_search(db, scantype, dbtype, no_refine, cpu, servermode):
         if scantype == SCANTYPE_MEM:
             dbpath, host, port = start_server(dbpath, host, port, end_port, cpu, dbtype)
 
-    # If server mode, just listen for connections and exit when interrupted
-    if servermode:
-        while True:
-            print(colorify("Server ready listening at %s:%s and using %d CPU cores" % (host, port, cpu), 'green'))
-            print(colorify("Use `emapper.py -d %s:%s:%s (...)` to search against this server" % (db, host, port), 'lblue'))
-            time.sleep(10)
-        raise emapperException("Server {db}:{host}:{port} stopped.")
-    else:
-        return dbname, dbpath, host, port, idmap_file, setup_type
+    return dbname, dbpath, host, port, idmap_file, setup_type
     
 ##
 def setup_eggnog_db(db, no_refine, scantype):
