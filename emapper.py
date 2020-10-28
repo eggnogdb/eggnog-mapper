8bbf9273978e3d14622fd143176e631af0531145
Cantalapiedra <cpcantalapiedra@gmail.com>
2020-10-28 18:55:09 +0100

    diamond default mode is diamond default (fast) mode

--- emapper.py
+++ emapper.py
@@ -10,7 +10,7 @@ sys.path.insert(0, SCRIPT_PATH)
 from eggnogmapper.emapperException import EmapperException
 from eggnogmapper.emapper import Emapper
 from eggnogmapper.search.search_modes import SEARCH_MODE_NO_SEARCH, SEARCH_MODE_DIAMOND, SEARCH_MODE_HMMER, SEARCH_MODE_MMSEQS2
-from eggnogmapper.search.diamond.diamond import SENSMODES, SENSMODE_MORE_SENSITIVE
+from eggnogmapper.search.diamond.diamond import SENSMODES, SENSMODE_FAST
 from eggnogmapper.search.hmmer.hmmer_search import QUERY_TYPE_SEQ, QUERY_TYPE_HMM, DB_TYPE_SEQ, DB_TYPE_HMM
 from eggnogmapper.annotation.pfam.pfam_modes import PFAM_TRANSFER_BEST_OG, PFAM_TRANSFER_NARROWEST_OG, PFAM_TRANSFER_SEED_ORTHOLOG, \
     PFAM_REALIGN_NONE, PFAM_REALIGN_REALIGN, PFAM_REALIGN_DENOVO
@@ -99,7 +99,7 @@ def create_arg_parser():
 
     pg_diamond.add_argument('--sensmode', dest='sensmode', 
                     choices = SENSMODES, 
-                    default=SENSMODE_MORE_SENSITIVE, help='Sensitive mode')
+                    default=SENSMODE_FAST, help='Sensitive mode')
     
     ##
     pg_mmseqs = parser.add_argument_group('MMseqs2 Search Options')
