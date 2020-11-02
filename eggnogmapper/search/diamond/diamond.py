77e7a12e609ecb45b530c4faf5605da21797e505
Cantalapiedra <cpcantalapiedra@gmail.com>
2020-11-02 21:35:10 +0100

    genepred from diamond searches

--- eggnogmapper/search/diamond/diamond.py
+++ eggnogmapper/search/diamond/diamond.py
@@ -8,7 +8,7 @@ from tempfile import mkdtemp
 import uuid
 
 from ...emapperException import EmapperException
-from ...common import DIAMOND, get_eggnog_dmnd_db, get_call_info
+from ...common import DIAMOND, get_eggnog_dmnd_db, get_call_info, ITYPE_CDS, ITYPE_PROTS, ITYPE_GENOME, ITYPE_META
 from ...utils import colorify
 
 from ..hmmer.hmmer_seqio import iter_fasta_seqs
@@ -34,6 +34,7 @@ class DiamondSearcher:
     score_thr = evalue_thr = query_cov = subject_cov = excluded_taxa = None
 
     in_file = None
+    itype = None
 
     # Results
     queries = hits = no_hits = None
@@ -41,10 +42,7 @@ class DiamondSearcher:
     ##
     def __init__(self, args):
         
-        if args.translate:
-            self.tool = 'blastx'
-        else:
-            self.tool = 'blastp'
+        self.itype = args.itype
 
         self.dmnd_db = args.dmnd_db if args.dmnd_db else get_eggnog_dmnd_db()
 
@@ -96,28 +94,35 @@ class DiamondSearcher:
 
     ##
     def get_hits(self):
+        return self.hits
+
+    ##
+    def get_no_hits(self):
         if self.hits is not None:
             hit_queries = set([x[0] for x in self.hits])
-            # no need to translate, we only need seq identifiers
             self.queries = set({name for name, seq in iter_fasta_seqs(self.in_file)})
-            # sequences = {name: seq for name, seq in iter_fasta_seqs(in_file, translate=self.traslate)}
-            # self.queries = set(sequences.keys())
             self.no_hits = set(self.queries).difference(hit_queries)
             
-        return self.hits, self.no_hits
+        return self.no_hits
 
     ##
     def run_diamond(self, fasta_file, output_file):
-
+        if self.itype == ITYPE_CDS or self.itype == ITYPE_GENOME or self.itype == ITYPE_META:
+            tool = 'blastx'
+        elif self.itype == ITYPE_PROTS:
+            tool = 'blastp'
+        else:
+            raise EmapperException(f"Unrecognized --itype {self.itype}.")
+        
         if self.sensmode == SENSMODE_FAST:
             cmd = (
-                f'{DIAMOND} {self.tool} -d {self.dmnd_db} -q {fasta_file} '
+                f'{DIAMOND} {tool} -d {self.dmnd_db} -q {fasta_file} '
                 f'--threads {self.cpu} -e {self.evalue_thr} -o {output_file} '
                 f'--query-cover {self.query_cov} --subject-cover {self.subject_cov}'
             )
         else:
             cmd = (
-                f'{DIAMOND} {self.tool} -d {self.dmnd_db} -q {fasta_file} '
+                f'{DIAMOND} {tool} -d {self.dmnd_db} -q {fasta_file} '
                 f'--{self.sensmode} --threads {self.cpu} -e {self.evalue_thr} -o {output_file} '
                 f'--query-cover {self.query_cov} --subject-cover {self.subject_cov}'
             )
@@ -125,9 +130,14 @@ class DiamondSearcher:
         if self.matrix: cmd += ' --matrix {self.matrix}'
         if self.gapopen: cmd += ' --gapopen {self.gapopen}'
         if self.gapextend: cmd += ' --gapextend {self.gapextend}'
-        
-        if self.excluded_taxa: cmd += " --max-target-seqs 25 "
-        else: cmd += " --top 3 "
+
+        if self.itype == ITYPE_CDS or self.itype == ITYPE_PROTS:
+            if self.excluded_taxa:
+                cmd += " --max-target-seqs 25 "
+            else:
+                cmd += " --top 3 "
+        else: # self.itype == ITYPE_GENOME or self.itype == ITYPE_META:
+            cmd += " --max-target-seqs 0 --max-hsps 0 "
 
         print(colorify('  '+cmd, 'yellow'))
         try:
@@ -137,8 +147,16 @@ class DiamondSearcher:
 
         return cmd
 
+
     ##
     def parse_diamond(self, raw_dmnd_file):
+        if self.itype == ITYPE_CDS or self.itype == ITYPE_PROTS:
+            return self._parse_diamond(raw_dmnd_file)
+        else: #self.itype == ITYPE_GENOME or self.itype == ITYPE_META:
+            return self._parse_genepred(raw_dmnd_file)
+        
+    ##
+    def _parse_diamond(self, raw_dmnd_file):
         hits = []
 
         visited = set()
@@ -168,8 +186,75 @@ class DiamondSearcher:
             
         return hits
 
+        ##
+    def _parse_genepred(self, raw_dmnd_file):
+        hits = []
+
+        visited = set()
+        with open(raw_dmnd_file, 'r') as raw_f:
+            for line in raw_f:
+                if not line.strip() or line.startswith('#'):
+                    continue
+
+                fields = list(map(str.strip, line.split('\t')))
+                # From diamond docs:
+                # By default, there are 12 preconfigured fields: qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
+                query = fields[0]
+                hit = fields[1]
+                qstart = int(fields[6])
+                qend = int(fields[7])
+                sstart = int(fields[8])
+                send = int(fields[9])
+                evalue = float(fields[10])
+                score = float(fields[11])
+                if evalue > self.evalue_thr or score < self.score_thr:
+                    continue
+
+                if self.excluded_taxa and hit.startswith("%s." % self.excluded_taxa):
+                    continue
+
+                hit = [query, hit, evalue, score, qstart, qend, sstart, send]
+
+                if not self._does_overlap(hit, hits):
+                    hits.append(hit)
+            
+        return hits
+
+    def _does_overlap(self, hit, hits):
+        does_overlap = False
+        
+        hitstart = hit[4]
+        hitend = hit[5]
+
+        for o in hits:
+            ostart = o[4]
+            oend = o[5]
+            overlap = None
+            if hitend <= ostart:
+                overlap = hitend - ostart
+            elif hitstart >= oend:
+                overlap = oend - hitstart
+            else:
+                overlap_start = max(hitstart, ostart)
+                overlap_end = min(hitend, oend)
+                overlap = overlap_end - (overlap_start - 1)
+
+            if overlap > 0:
+                does_overlap = True
+                break
+                
+        return does_overlap
+
+
     ##
     def output_diamond(self, cmd, hits, out_file):
+        if self.itype == ITYPE_CDS or self.itype == ITYPE_PROTS:
+            return self._output_diamond(cmd, hits, out_file)
+        else: #self.itype == ITYPE_GENOME or self.itype == ITYPE_META:
+            return self._output_genepred(cmd, hits, out_file)
+        
+    ##
+    def _output_diamond(self, cmd, hits, out_file):
         with open(out_file, 'w') as OUT:
         
             if not self.no_file_comments:
@@ -178,6 +263,31 @@ class DiamondSearcher:
 
             for line in hits:
                 print('\t'.join(map(str, line)), file=OUT)
+                
+        return
+
+    ##
+    def _output_genepred(self, cmd, hits, out_file):
+        queries_suffixes = {}
+        with open(out_file, 'w') as OUT:
+        
+            if not self.no_file_comments:
+                print(get_call_info(), file=OUT)
+                print('#'+cmd, file=OUT)
+
+            for line in hits:
+                query = line[0]
+                target = line[1]
+                evalue = line[2]
+                score = line[3]
+                if query in queries_suffixes:
+                    queries_suffixes[query] += 1
+                    suffix = queries_suffixes[query]
+                else:
+                    suffix = 0
+                    queries_suffixes[query] = suffix
+                    
+                print('\t'.join(map(str, [f"{query}_{suffix}", target, str(evalue), str(score)])), file=OUT)
                 
         return
 
