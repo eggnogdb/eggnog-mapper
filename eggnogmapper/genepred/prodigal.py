d7cd069f7ccaac16e5540796d3b7302fc2a31930
Cantalapiedra <cpcantalapiedra@gmail.com>
2021-02-06 12:01:30 +0100

    Prodigal training mode

--- eggnogmapper/genepred/prodigal.py
+++ eggnogmapper/genepred/prodigal.py
@@ -1,6 +1,7 @@
 ##
 ## CPCantalapiedra 2020
 
+from os.path import isfile
 from os.path import join as pjoin
 import shutil
 import subprocess
@@ -19,6 +20,7 @@ class ProdigalPredictor:
     cpu = None
 
     trans_table = None
+    training_genome = training_file = None
     
     outdir = None # dir with prodigal out files
     outgff = outprots = outcds = outorfs = None # prodigal out files
@@ -38,6 +40,8 @@ class ProdigalPredictor:
         self.cpu = args.cpu
 
         self.trans_table = args.trans_table
+        self.training_genome = args.training_genome
+        self.training_file = args.training_file
         
         self.temp_dir = args.temp_dir
         
@@ -49,6 +53,15 @@ class ProdigalPredictor:
 
         self.outdir = mkdtemp(prefix='emappertmp_prod_', dir=self.temp_dir)
         try:
+            # Training: run only if the training file does NOT exist
+            if self.training_genome is not None and self.training_file is not None:
+                if isfile(self.training_file):
+                    print(colorify(f'Warning: --training_file {self.training_file} already exists. '
+                                   f'Training will be skipped, and prediction will be run using the existing training file.', 'red'))                                    
+                else:
+                    cmd = self.run_training(self.training_genome, self.training_file, self.outdir)
+
+            # Gene prediction
             cmd = self.run_prodigal(in_file, self.outdir)
 
         except Exception as e:
@@ -61,6 +74,22 @@ class ProdigalPredictor:
         shutil.rmtree(self.outdir)
         return
 
+    def run_training(self, in_file, training_file, outdir):
+        cmd = (
+            f'{PRODIGAL} -i {in_file} -t {training_file}'
+        )
+
+        if self.trans_table is not None:
+            cmd += f' -g {self.trans_table}'
+
+        print(colorify('  '+cmd, 'yellow'))
+        try:
+            completed_process = subprocess.run(cmd, capture_output=True, check=True, shell=True)
+        except subprocess.CalledProcessError as cpe:
+            raise EmapperException("Error running prodigal: "+cpe.stderr.decode("utf-8").strip().split("\n")[-1])
+
+        return cmd
+    
     def run_prodigal(self, in_file, outdir):
         self.outfile = pjoin(outdir, "output.gff")
         self.outprots = pjoin(outdir, "output.faa")
@@ -78,6 +107,12 @@ class ProdigalPredictor:
                 print(colorify(f'Warning: --trans_table (-g Prodigal option) '
                                f'is ignored by Prodigal when using -p {self.PMODE_META}', 'red'))                
             cmd += f' -g {self.trans_table}'
+
+        if self.training_file is not None and isfile(self.training_file):
+            if self.pmode == self.PMODE_META:
+                print(colorify(f'Warning: Ignoring --training_file, because Prodigal does not allow training for -p {self.PMODE_META} ', 'red'))                
+            else:
+                cmd += f' -t {self.training_file}'
 
         print(colorify('  '+cmd, 'yellow'))
         try:
