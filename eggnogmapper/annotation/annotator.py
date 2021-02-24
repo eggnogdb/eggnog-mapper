b89e79f29c573dcb6136dd8ff92986cc7f749b21
Cantalapiedra <cpcantalapiedra@gmail.com>
2021-02-24 10:39:35 +0100

    ncbi fresh on disk

--- eggnogmapper/annotation/annotator.py
+++ eggnogmapper/annotation/annotator.py
@@ -278,7 +278,7 @@ class Annotator:
         def init_db():
             global eggnog_db, ncbi
             eggnog_db = get_fresh_eggnog_db(usemem = False)
-            ncbi = get_ncbi(usemem = True)
+            ncbi = get_fresh_ncbi(usemem = False)
             return
         
         pool = multiprocessing.Pool(self.cpu, init_db, ())
