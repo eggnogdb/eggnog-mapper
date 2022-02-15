c5a4c4b3f99c89a32b106adbbdeed8967dfa954d
Cantalapiedra <cpcantalapiedra@gmail.com>
2022-02-15 17:52:47 +0100

    blastx gff

--- eggnogmapper/emapper.py
+++ eggnogmapper/emapper.py
@@ -237,11 +237,17 @@ class Emapper:
     def decorate_gff_f(self, args, predictor, searcher_name, annotated_hits):
 
         gff_outfile = pjoin(self._current_dir, self.deco_gff_file)
-        gff_genepred_file = predictor.outgff
+        if predictor is not None:
+            gff_genepred_file = predictor.outgff
+            gff_genepred_fasta = predictor.outprots
+        else:
+            gff_genepred_file = None
+            gff_genepred_fasta = None
+        
         
         annotated_hits = run_gff_decoration(self.decorate_gff, self.resume, args.decorate_gff_ID_field,
                                             self.genepred_is_prodigal, self.genepred_is_blastx,
-                                            gff_genepred_file, gff_outfile,
+                                            gff_genepred_file, gff_genepred_fasta, gff_outfile,
                                             predictor, searcher_name, annotated_hits)
         
         return annotated_hits
