0613295dbb88c91369924c6d8c00c16c6ee456f7
Cantalapiedra <cpcantalapiedra@gmail.com>
2022-02-16 18:41:17 +0100

    resume

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -559,11 +559,11 @@ class Test(unittest.TestCase):
         '''
         Test gene prediction with diamond
         '''
-        rm -r tmp_test; mkdir tmp_test;
-        emapper.py -i tests/fixtures/genepred_contig/contig.fna --itype metagenome --genepred search \
-            --data_dir tests/fixtures \
-            -m diamond --sensmode sensitive --no_annot --dmnd_db tests/fixtures/genepred_contig/contig.dmnd \
-            -o test --output_dir tmp_test
+        # rm -r tmp_test; mkdir tmp_test;
+        # emapper.py -i tests/fixtures/genepred_contig/contig.fna --itype metagenome --genepred search \
+        #     --data_dir tests/fixtures \
+        #     -m diamond --sensmode sensitive --no_annot --dmnd_db tests/fixtures/genepred_contig/contig.dmnd \
+        #     -o test --output_dir tmp_test
         
         ##
         # Setup test
