8ab5cfcdbd0b56209ed33133e514aaf5850303b9
Cantalapiedra <cpcantalapiedra@gmail.com>
2023-02-09 08:48:06 +0100

    prodigal gz exception

--- eggnogmapper/genepred/prodigal.py
+++ eggnogmapper/genepred/prodigal.py
@@ -83,6 +83,8 @@ class ProdigalPredictor:
 
     def run_training(self, in_file, training_file):
 
+        print(colorify(in_file, 'red'))
+        
         if in_file.endswith(".gz") or in_files.endswith(".gz\'"):
             raise EmapperException("Error running prodigal. gzipped files are not allowed.")
         
